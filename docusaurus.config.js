de44d43f43289c32d0503d38e72bdc865b8afee4
Hu Chunxu <huchunxu@ps-micro.com>
2023-06-21 21:08:33 +0800

    Update github address

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/HorizonRDK/rdk_x3_doc/blob/develop/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/HorizonRDK/rdk_x3_doc/blob/develop/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
