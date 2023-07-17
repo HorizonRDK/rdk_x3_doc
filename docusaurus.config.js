4cf96357bf3f1e5cf1924a7804b59ce49258aa6b
nuo.wu <nuo.wu@horizon.ai>
2023-07-17 19:39:28 +0800

    Modify baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/home/wunuo/docusaurus/RDK-X3/build',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
