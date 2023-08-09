6368cd34aea4ff78d52ed17a9b716fe697531953
xuguosheng110 <30901008+xuguosheng110@users.noreply.github.com>
2023-08-09 11:06:25 +0800

    change baseUrl for auto deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'https://developer.horizon.cc/documents_rdk/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
